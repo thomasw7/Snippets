05206946e6fc5d27e2a51643cfc1a8576af2cc64
Thomas <thomasw713@gmail.com>
2020-07-13 08:50:28 -0400

    add back solver utility

---
+++ Snippets.Utilities.Tests/Solver/SolverUtilityTest.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using Snippets.Utilities.Solver;
+using System;
+
+namespace Snippets.Utilities.Tests.Solver
+{
+    public class SolverUtilityTest
+    {
+        [Test]
+        public void TestSolve()
+        {
+            //
+            var expected = 8;
+            var utility = new SolverUtility();
+
+            //
+            var actual = Math.Round(utility.Solve(x => Math.Pow(x, 2), 64, 6),6);
+
+            //
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        [TestCase(ExpectedResult = "Unable to converge. Goal: 2, Tolerance: 1E-05.")]
+        public string TestForException()
+        {
+            //
+            var utility = new SolverUtility();
+
+            //
+            TestDelegate testDelegate = () => utility.Solve(x => 1, 2, 1);
+
+            //
+            var ex = Assert.Throws<Exception>(testDelegate);
+            return ex.Message;
+        }
+    }
+}
---
+++ Snippets.Utilities/Solver/SolverUtility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snippets.Utilities.Solver
+{
+    public class SolverUtility
+    {
+        public double Solve(Func<double, double> function, double goal, double trial, double epsilon = 0.000001, double tolerance = 0.00001, int iterations = 100)
+        {
+            if (iterations < 1)
+            {
+                throw new Exception($"Unable to converge. Goal: {goal}, Tolerance: {tolerance}.");
+            }
+
+            var result = function(trial);
+
+            if (Math.Abs(goal - result) < tolerance)
+            {
+                return trial;
+            }
+
+            var delta = function(trial + epsilon) - result;
+
+            var approx = trial + epsilon * ((goal - result) / delta);
+
+            return Solve(function, goal, approx, epsilon, tolerance, iterations - 1);
+        }
+    }
+}
